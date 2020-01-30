617d42e419b9cf6f45c7e8ba2845044616aefcc5
sofn <lishaofeng02@meituan.com>
2020-01-30 14:10:30 +0800

    区分测试、线上环境

--- src/net/common.ts
+++ src/net/common.ts
@@ -23,7 +23,8 @@ export const instance = axios.create({
 
 function getBaseUrl(host?: string) {
   const windowHost = host || window.location.host;
-  return `${document.location.protocol}//${windowHost}/api`;
+  let isDev = windowHost === "localhost" || windowHost === "127.0.0.1";
+  return `${document.location.protocol}//${windowHost}`+(isDev ? `/api`: ``);
 }
 
 export function get(url: string, param: any) {
